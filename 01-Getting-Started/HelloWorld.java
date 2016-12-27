f757128d8d695644bb325bed80c11b28671dc38a
StephanieAngelaBrauer <S.Angela.Brauer@gmail.com>
2016-12-28 10:21:56 +1100

    Uploading my first Java program

---
+++ 01-Getting-Started/HelloWorld.java
@@ -0,0 +1,7 @@
+
+public class HelloWorld {
+  public static void main (String args[]) {
+	System.out.println("Hello World!");
+	System.out.println("My name is Stephanie-Angela Brauer. This is my first Java program.");
+  }
+}
