91594fdf74890f7eb2f1c57252d1f41ef33e1cef
StephanieAngelaBrauer <S.Angela.Brauer@gmail.com>
2016-12-28 16:08:03 +1100

    01-Getting-Started: Update

--- 01-Getting-Started/HelloWorld.java
+++ 01-Getting-Started/HelloWorld.java
@@ -1,13 +1,14 @@
 /**
 
 	This is Hello World, my first Java program.
+
 	@author Stephanie-Angela Brauer
 	
 */
 
 public class HelloWorld {
-  public static void main (String args[]) {
-	System.out.println("Hello World!");
-	System.out.println("My name is Stephanie-Angela Brauer. This is my first Java program.");
-  }
+	public static void main (String args[]) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Stephanie-Angela Brauer. This is my first Java program.");
+	}
 }
